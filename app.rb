212f9bf7488b7944c46a8125af67dce3060235d4
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-03-06 19:39:42 +0000

    Done, with no bells *or* whistles

--- app.rb
+++ app.rb
@@ -1,8 +1,56 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:main)
+
+end
+
+get("/rock") do
+  rock = "rock"
+  @roll = ["rock", "paper", "scissors"].sample
+
+  if @roll == "rock"
+    @result = "tied"
+  elsif @roll == "paper"
+    @result = "lost"
+  elsif @roll == "scissors"
+    @result = "won"
+  end
+
+  erb(:rock)
+
+end
+
+get("/paper") do
+  paper = "paper"
+  @roll = ["rock", "paper", "scissors"].sample
+
+  if @roll == "rock"
+    @result = "won"
+  elsif @roll == "paper"
+    @result = "tied"
+  elsif @roll == "scissors"
+    @result = "lost"
+  end
+
+  erb(:paper)
+
+end
+
+get("/scissors") do
+  scissors = "scissors"
+  @roll = ["rock", "paper", "scissors"].sample
+
+  if @roll == "rock"
+    @result = "lost"
+  elsif @roll == "paper"
+    @result = "won"
+  elsif @roll == "scissors"
+    @result = "tied"
+  end
+
+  erb(:scissors)
+
 end
